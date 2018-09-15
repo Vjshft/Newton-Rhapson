b6c059f0a32345c2ebd5662c5ab198050f5daa19
Vjshft <switch88vjshft@gmail.com>
2018-09-14 23:05:13 -0500

    Add files via upload
    Codigo de Newton-Raphson en Java con comentarios

--- newton.java
+++ newton.java
@@ -4,24 +4,26 @@ import javax.swing.JOptionPane;
 
 public class newton {
 	public static void main(String[] args) {
-		double contador = 0;
+        //inicializa las variables de contador Valor Anterior, Valor actual, x0, xi y el porcentaje de error.
+		double contador = 0;   
 		double valorAnterior = 0;
 		double valorActual, xo, xi;
-		double ea = 100;
-	    double fxo, fo;
+		double ea = 100; //porcentaje de error
+	    double fxo, fo; //valor de y en las funcion original y la derivada
+        //se pregunta al usuario por el porcentaje de error minimo aceptado y la x con la que empieza a correr el programa 
 	    double es = Double.parseDouble(JOptionPane.showInputDialog("Dame el % minimo aceptado"));
 	    xo = Double.parseDouble(JOptionPane.showInputDialog("Dame Xo"));
 	    
 		do{
-			fxo = Funcion(xo);
-		    fo = FuncionDer(xo);
-			xi = xo - (fxo / fo);
+			fxo = Funcion(xo); //obtiene el valor de y al evaluar x0 en la funcion
+		    fo = FuncionDer(xo); //obtiene el valor de y al evaluar x0 en la funcion derivada
+			xi = xo - (fxo / fo); 
 	        valorActual = xi;
 	        valorAnterior = xo;
 	        System.out.println("Xo: " + xo);
 	        System.out.println("Xi: " + xi);
-	        ea = ((valorActual - valorAnterior) / valorActual) * 100;
-	        ea = Math.abs(ea);
+	        ea = ((valorActual - valorAnterior) / valorActual) * 100; //calcula el porcentaje de error nuevo
+	        ea = Math.abs(ea); 
 	        System.out.println("Ea: " + ea);
 	        System.out.println("Es: " + es);
 	        xo = xi;
@@ -29,20 +31,18 @@ public class newton {
 	        System.out.println("Iteracion: " + contador);
 	        System.out.println("");
 		}
-		while (ea > es);
-		System.out.println("La raiz es: " + xi);
+		while (ea > es); //realiza n numero de iteraciones mientras el porcentaje de error sea mayor al aceptado
+		System.out.println("La raiz es: " + xi); //imprime la raiz que se obtuvo
 	}
 	
 	public static double Funcion(double x){
-		double res = ((-0.6)*Math.pow(x, 2))+(2.4*x)+5.5;
-		//double res = (x/(1-x))*Math.sqrt(7/2+x) - 0.04;
+    double res = ((-0.6)*Math.pow(x, 2))+(2.4*x)+5.5; //Ejemplo de una funcion cuadratica -0.06x^2 + 2.4x + 5.5
 		return res; 
 		
 	}
 	
 	public static double FuncionDer(double x){
-		double res = -1.2*x+2.4;
-		//double res = (Math.sqrt(7)*(Math.pow(x, 2)+x+4))/(2*Math.pow(1-x, 2)*Math.pow(x+2, (3/2)));
+		double res = -1.2*x+2.4;  //Derivada de la funcion cuadratica dando como resultado -1.2x + 2.4 
 		return res;
 	}
 }
